6e7ac8213a34a757dacb4420cdd1479eb7164498
GrifMcPo <brawltvink6@bk.ru>
2026-08-16 00:26:46 +0300

    Create KeyBindings.java

---
+++ src/main/java/com/readvisuals/keybind/KeyBindings.java
@@ -0,0 +1,19 @@
+package com.readvisuals.keybind;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBindings {
+    public static KeyBinding OPEN_MENU;
+
+    public static void register() {
+        OPEN_MENU = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.readvisuals.open_menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_K,
+                "category.readvisuals"
+        ));
+    }
+}
